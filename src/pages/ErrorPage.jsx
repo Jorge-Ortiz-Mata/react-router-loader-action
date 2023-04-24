4132494cb0f0a6485c6a1f5bb2c9f2e519926f16
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-04-24 14:06:45 -0500

    Adding different error responses.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,13 +2,20 @@ import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
+  let message;
 
-  console.log(error);
+  if(error.status === 500){
+    message = JSON.parse(error.data).message;
+  }
+
+  if(error.status === 404){
+    message = error.data;
+  }
 
   return(
     <>
       <h1 className="text-center font-bold text-2xl">An error ocurred.</h1>
-      <p className="text-center font-semibold">{JSON.parse(error.data).message}</p>
+      <p className="text-center font-semibold">{message}</p>
     </>
   )
 }
